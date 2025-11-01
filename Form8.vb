1ef5b102230f1225f45c47774337a579d4c6e05a
vivek gupta <vivekgupta4344@gmail.com>
2025-11-01 12:24:00 +0530

    Add files via upload

---
+++ Form8.vb
@@ -0,0 +1,46 @@
+﻿Imports MySql.Data.MySqlClient
+Imports Windows.Win32.System
+
+Public Class Form8
+    Dim con As New MySqlConnection
+    Dim cmd As New MySqlCommand
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Try
+
+            Dim cmd = con.CreateCommand()
+            cmd.CommandType = CommandType.Text
+            cmd.CommandText = "delete from alldetail.about where userid='" + TextBox1.Text + "'"
+            cmd.ExecuteNonQuery()
+            Dim dt As New DataTable
+            Dim da As New MySqlDataAdapter(cmd)
+            da.Fill(dt)
+            MessageBox.Show("Record deleted successfully!!")
+        Catch ex As Exception
+
+            MessageBox.Show("An error occurred: " & ex.Message)
+        End Try
+
+    End Sub
+
+    Private Sub Form8_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        con.ConnectionString = "server=localhost; user id=root; password=vivek; database=alldetail;  "
+        If con.State = ConnectionState.Open Then
+            con.Close()
+
+        End If
+
+        con.Open()
+    End Sub
+
+    Private Sub PictureBox2_Click(sender As Object, e As EventArgs) Handles PictureBox2.Click
+        Me.Close()
+    End Sub
+End Class
