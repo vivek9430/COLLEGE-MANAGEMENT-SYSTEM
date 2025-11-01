fc2ba9001185cb1d6bd3b52c948d3459e88a6585
vivek gupta <vivekgupta4344@gmail.com>
2025-11-01 12:21:39 +0530

    Add files via upload

---
+++ Form4.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form4
+    Private Sub Form4_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Label4_Click(sender As Object, e As EventArgs) Handles Label4.Click
+
+    End Sub
+End Class
