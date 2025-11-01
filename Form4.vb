e4c6d6159a4ea5f45ee1d960953f0bd2bfb8a433
vivek gupta <vivekgupta4344@gmail.com>
2025-11-01 13:06:07 +0530

    Delete Form4.vb

--- Form4.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class Form4
-    Private Sub Form4_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-    End Sub
-
-    Private Sub Label4_Click(sender As Object, e As EventArgs) Handles Label4.Click
-
-    End Sub
-End Class
