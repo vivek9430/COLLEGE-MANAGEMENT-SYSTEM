db987fcbc95b8dd10f632a8f27f46b5c3b90b903
vivek gupta <vivekgupta4344@gmail.com>
2025-11-06 21:57:58 +0530

    Add files via upload

---
+++ Form19.vb
@@ -0,0 +1,71 @@
+﻿Imports MySql.Data.MySqlClient
+Public Class Form19
+    Dim con As New MySqlConnection
+    Dim cmd As New MySqlCommand
+    Private Sub Form19_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        con.ConnectionString = "server=localhost; user id=root; password=vivek; database=teacherfees;  "
+        If con.State = ConnectionState.Open Then
+            con.Close()
+
+        End If
+
+        con.Open()
+        connect()
+    End Sub
+    Public Sub connect()
+        cmd = con.CreateCommand
+        cmd.CommandType = CommandType.Text
+        cmd.CommandText = "select * from teacherfees.paid"
+        cmd.ExecuteNonQuery()
+        Dim dt As New DataTable
+        Dim da As New MySqlDataAdapter(cmd)
+        da.Fill(dt)
+        DataGridView1.DataSource = dt
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        cmd = con.CreateCommand
+        cmd.CommandType = CommandType.Text
+        cmd.CommandText = "insert into paid values('" + TextBox1.Text + "','" + TextBox2.Text + "','" + TextBox3.Text + "','" + DateTimePicker1.Text + "','" + TextBox4.Text + "')"
+        cmd.ExecuteNonQuery
+        connect
+        TextBox1.Text = ""
+        TextBox2.Text = ""
+        TextBox3.Text = ""
+        DateTimePicker1.Text = ""
+        TextBox4.Text = ""
+    End Sub
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        cmd = con.CreateCommand
+        cmd.CommandType = CommandType.Text
+        cmd.CommandText = "select * from teacherfees.paid where userid='" + TextBox5.Text + "'"
+        cmd.ExecuteNonQuery()
+        Dim dt As New DataTable
+        Dim da As New MySqlDataAdapter(cmd)
+        da.Fill(dt)
+        DataGridView1.DataSource = dt
+
+    End Sub
+
+    Private Sub PictureBox3_Click(sender As Object, e As EventArgs) Handles PictureBox3.Click
+        Me.Close()
+    End Sub
+
+    Private Sub PrintDocument1_PrintPage(sender As Object, e As Printing.PrintPageEventArgs) Handles PrintDocument1.PrintPage
+        Dim imagebmp As New Bitmap(Me.DataGridView1.Width, Me.DataGridView1.Height)
+        DataGridView1.DrawToBitmap(imagebmp, New Rectangle(0, 0, Me.DataGridView1.Width, Me.DataGridView1.Height))
+        e.Graphics.DrawImage(imagebmp, 0, 0)
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        PrintPreviewDialog1.Document = PrintDocument1
+        PrintPreviewDialog1.PrintPreviewControl.Zoom = 1
+        PrintPreviewDialog1.ShowDialog()
+    End Sub
+End Class
