b24b9a309ae7c1daa2bca513ba49bfec7e0d69ca
vivek gupta <vivekgupta4344@gmail.com>
2025-11-06 21:57:30 +0530

    Add files via upload

---
+++ Form18.vb
@@ -0,0 +1,42 @@
+﻿Imports MySql.Data.MySqlClient
+Imports Mysqlx.XDevAPI
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+Imports Windows.Win32.System
+
+Public Class Form18
+    Dim con As New MySqlConnection
+    Dim cmd As New MySqlCommand
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        cmd = con.CreateCommand()
+        cmd.CommandType = CommandType.Text
+        cmd.CommandText = "UPDATE alldetail.about SET semester = '" + ComboBox2.Text + "' WHERE userid = '" + TextBox2.Text + "'"
+        cmd.ExecuteNonQuery()
+        Dim dt As New DataTable
+        Dim da As New MySqlDataAdapter(cmd)
+        da.Fill(dt)
+        MessageBox.Show("record updated")
+    End Sub
+
+    Private Sub Form18_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        con.ConnectionString = "server=localhost; user id=root; password=vivek; database=alldetail;  "
+        If con.State = ConnectionState.Open Then
+            con.Close()
+
+        End If
+
+        con.Open()
+    End Sub
+    Public Sub connect()
+        cmd = con.CreateCommand
+        cmd.CommandType = CommandType.Text
+        cmd.CommandText = "select * from alldetail.about"
+        cmd.ExecuteNonQuery()
+        Dim dt As New DataTable
+        Dim da As New MySqlDataAdapter(cmd)
+        da.Fill(dt)
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+        Me.Close()
+    End Sub
+End Class
