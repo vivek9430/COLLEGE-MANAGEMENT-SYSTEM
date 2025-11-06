ec5a5958de8016c60903a450ec47cc1614071cf1
vivek gupta <vivekgupta4344@gmail.com>
2025-11-06 21:59:00 +0530

    Add files via upload

---
+++ Form21.vb
@@ -0,0 +1,28 @@
+﻿Imports MySql.Data.MySqlClient
+Public Class Form21
+
+
+
+
+
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs)
+
+    End Sub
+
+    Private Sub DataGridView2_CellContentClick(sender As Object, e As DataGridViewCellEventArgs)
+
+    End Sub
+
+
+
+
+
+    Private Sub PrintPreviewDialog1_Load(sender As Object, e As EventArgs) Handles PrintPreviewDialog1.Load
+
+    End Sub
+
+    Private Sub Form21_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
