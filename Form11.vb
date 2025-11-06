640a42961af88525efee0e8cd5d49cdf6e1b9831
vivek gupta <vivekgupta4344@gmail.com>
2025-11-06 21:53:14 +0530

    Add files via upload

---
+++ Form11.vb
@@ -0,0 +1,69 @@
+﻿
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+Imports Windows.Win32.System
+Imports MySql.Data.MySqlClient
+
+Public Class Form11
+    Dim con As New MySqlConnection
+    Dim cmd As New MySqlCommand
+    Private Sub Form11_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        con.ConnectionString = "server=localhost; user id=root; password=vivek; database=alldetail;  "
+        If con.State = ConnectionState.Open Then
+            con.Close()
+
+        End If
+
+        con.Open()
+
+    End Sub
+    Public Sub connect()
+        cmd = con.CreateCommand
+        cmd.CommandType = CommandType.Text
+        cmd.CommandText = "select * from alldetail.about"
+        cmd.ExecuteNonQuery()
+        Dim dt As New DataTable
+        Dim da As New MySqlDataAdapter(cmd)
+        da.Fill(dt)
+
+    End Sub
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
+        cmd = con.CreateCommand
+        cmd.CommandType = CommandType.Text
+        cmd.CommandText = "insert into about values('" + TextBox1.Text + "','" + TextBox2.Text + "','" + TextBox3.Text + "','" + ComboBox1.Text + "','" + ComboBox1.Text + "')"
+        cmd.ExecuteNonQuery()
+        connect()
+        TextBox1.Text = ""
+        TextBox2.Text = ""
+        TextBox3.Text = ""
+        ComboBox1.Text = ""
+        ComboBox1.Text = ""
+
+
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+        Me.Close()
+    End Sub
+
+    Private Sub ComboBox2_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox2.SelectedIndexChanged
+
+    End Sub
+End Class
