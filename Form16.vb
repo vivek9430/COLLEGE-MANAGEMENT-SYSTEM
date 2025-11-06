984dc8efd2b9d9470bb0cf75c47cc826f3cbcadf
vivek gupta <vivekgupta4344@gmail.com>
2025-11-06 21:56:30 +0530

    Add files via upload

---
+++ Form16.vb
@@ -0,0 +1,41 @@
+﻿Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+Imports MySql.Data.MySqlClient
+Public Class Form16
+    Dim con As New MySqlConnection
+    Dim cmd As New MySqlCommand
+    Private Sub Form16_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        con.ConnectionString = "server=localhost; user id=root; password=vivek; database=miscellaneous;  "
+        If con.State = ConnectionState.Open Then
+            con.Close()
+
+        End If
+
+        con.Open()
+    End Sub
+    Public Sub connect()
+        cmd = con.CreateCommand
+        cmd.CommandType = CommandType.Text
+        cmd.CommandText = "select * from miscellaneous.additem"
+        cmd.ExecuteNonQuery()
+        Dim dt As New DataTable
+        Dim da As New MySqlDataAdapter(cmd)
+        da.Fill(dt)
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        cmd = con.CreateCommand
+        cmd.CommandType = CommandType.Text
+        cmd.CommandText = "insert into additem values('" + TextBox1.Text + "','" + TextBox2.Text + "','" + TextBox3.Text + "')"
+        cmd.ExecuteNonQuery()
+        connect()
+        TextBox1.Text = ""
+        TextBox2.Text = ""
+        TextBox3.Text = ""
+
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+        Me.Close()
+    End Sub
+End Class
