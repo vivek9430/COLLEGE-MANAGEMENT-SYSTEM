9f41932d727aba216834d8d5eba9debdfd19a62f
vivek gupta <vivekgupta4344@gmail.com>
2025-11-06 21:55:03 +0530

    Add files via upload

---
+++ Form13.vb
@@ -0,0 +1,35 @@
+﻿Imports MySql.Data.MySqlClient
+Imports Windows.Win32.System
+
+Public Class Form13
+    Dim con As New MySqlConnection
+    Dim cmd As New MySqlCommand
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        cmd = con.CreateCommand()
+        cmd.CommandType = CommandType.Text
+        cmd.CommandText = "delete from teacher.information where userid='" + TextBox1.Text + "'"
+        cmd.ExecuteNonQuery()
+        Dim dt As New DataTable
+        Dim da As New MySqlDataAdapter(cmd)
+        da.Fill(dt)
+        MessageBox.Show("record deleted successfully!!")
+    End Sub
+
+    Private Sub Form13_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        con.ConnectionString = "server=localhost; user id=root; password=vivek; database=teacher;  "
+        If con.State = ConnectionState.Open Then
+            con.Close()
+
+        End If
+
+        con.Open()
+    End Sub
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+
+    End Sub
+
+    Private Sub PictureBox2_Click(sender As Object, e As EventArgs) Handles PictureBox2.Click
+        Me.Close()
+    End Sub
+End Class
